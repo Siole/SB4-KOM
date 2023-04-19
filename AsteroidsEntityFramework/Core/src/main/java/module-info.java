34aa4e82e4b2534c9e2fdcfd49a67288ba06fff0
Siole <siole21@student.sdu.dk>
2023-04-19 13:30:51 +0200

    adding annotations

--- AsteroidsEntityFramework/Core/src/main/java/module-info.java
+++ AsteroidsEntityFramework/Core/src/main/java/module-info.java
@@ -5,6 +5,10 @@ module Core {
     requires CommonAsteroids;
     requires java.desktop;
     requires com.badlogic.gdx;
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+    opens dk.sdu.mmmi.cbse.main to spring.core;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
