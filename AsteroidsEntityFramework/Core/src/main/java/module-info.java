0505c44d20a79989a6ebc3c0d81adb5df61cd005
Siole <siole21@student.sdu.dk>
2023-04-19 14:01:51 +0200

    update module info in core

--- AsteroidsEntityFramework/Core/src/main/java/module-info.java
+++ AsteroidsEntityFramework/Core/src/main/java/module-info.java
@@ -8,6 +8,7 @@ module Core {
     requires spring.context;
     requires spring.core;
     requires spring.beans;
+    exports dk.sdu.mmmi.cbse.main;
     opens dk.sdu.mmmi.cbse.main to spring.core;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
