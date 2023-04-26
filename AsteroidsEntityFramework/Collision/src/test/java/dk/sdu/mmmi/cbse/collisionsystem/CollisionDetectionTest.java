00f3bc669104d1c8045705e53e37073643d6c697
Siole <siole21@student.sdu.dk>
2023-04-26 09:04:13 +0200

    Junit setup

---
+++ AsteroidsEntityFramework/Collision/src/test/java/dk/sdu/mmmi/cbse/collisionsystem/CollisionDetectionTest.java
@@ -0,0 +1,22 @@
+package dk.sdu.mmmi.cbse.collisionsystem;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CollisionDetectionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void collision() {
+    }
+}
