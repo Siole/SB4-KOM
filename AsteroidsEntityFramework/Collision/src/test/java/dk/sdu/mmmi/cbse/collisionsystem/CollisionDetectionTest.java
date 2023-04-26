1a94196aa125be5fbad932b4190b85d3407465af
Siole <siole21@student.sdu.dk>
2023-04-26 10:07:24 +0200

    collision test

--- AsteroidsEntityFramework/Collision/src/test/java/dk/sdu/mmmi/cbse/collisionsystem/CollisionDetectionTest.java
+++ AsteroidsEntityFramework/Collision/src/test/java/dk/sdu/mmmi/cbse/collisionsystem/CollisionDetectionTest.java
@@ -1,5 +1,6 @@
 package dk.sdu.mmmi.cbse.collisionsystem;
 
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,9 +15,17 @@ class CollisionDetectionTest {
 
     @AfterEach
     void tearDown() {
+
     }
 
     @Test
     void collision() {
+        Entity entity1 = new Entity();
+        Entity entity2 = new Entity();
+        entity1.setRadius(10);
+        entity2.setRadius(10);
+        CollisionDetection collisionDetection = new CollisionDetection();
+        boolean result = collisionDetection.collision(entity1, entity2);
+        assertEquals(false, result);
     }
 }
