e1c0f8b9089ed6467e9a69029d05ccb61f6dc9c3
Siole <siole21@student.sdu.dk>
2023-04-26 10:50:39 +0200

    Unit test working

--- AsteroidsEntityFramework/Collision/src/test/java/dk/sdu/mmmi/cbse/collisionsystem/CollisionDetectionTest.java
+++ AsteroidsEntityFramework/Collision/src/test/java/dk/sdu/mmmi/cbse/collisionsystem/CollisionDetectionTest.java
@@ -1,6 +1,7 @@
 package dk.sdu.mmmi.cbse.collisionsystem;
 
 import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,11 +20,26 @@ class CollisionDetectionTest {
     }
 
     @Test
-    void collision() {
+    void collisiontrue() {
         Entity entity1 = new Entity();
         Entity entity2 = new Entity();
         entity1.setRadius(10);
         entity2.setRadius(10);
+        entity1.add(new PositionPart(10,10,10));
+        entity2.add(new PositionPart(10,10,10));
+        CollisionDetection collisionDetection = new CollisionDetection();
+        boolean result = collisionDetection.collision(entity1, entity2);
+        assertEquals(true, result);
+    }
+
+    @Test
+    void collisionfalse() {
+        Entity entity1 = new Entity();
+        Entity entity2 = new Entity();
+        entity1.setRadius(10);
+        entity2.setRadius(10);
+        entity1.add(new PositionPart(10,10,10));
+        entity2.add(new PositionPart(100,100,10));
         CollisionDetection collisionDetection = new CollisionDetection();
         boolean result = collisionDetection.collision(entity1, entity2);
         assertEquals(false, result);
